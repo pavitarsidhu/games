0c90c0546fe281019ab5cb02e0ac8b03273b760b
Pavitar Sidhu <pav_sidhu@live.ca>
2019-02-10 20:16:08 -0800

    Delete SnakesAndLadder.java

--- SnakesAndLadder.java
+++
@@ -1,8 +0,0 @@
-import GameBoard;
-
-public class SnakesAndLadder {
-	public static void main (String[] args) {
-		Gameboard game = new GameBoard(new int[]{0,0,0,0,0,-3,3,-5,4,0,0, 3, 4, -5, -4, 0,0,0,0,0,0,0,0,0,-4,4,0,0});
-		game.startGame();
-	}
-}
